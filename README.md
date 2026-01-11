eeabafec1abd0b13166a26ef9770fbe0cee40f70
krcb197 <34693973+krcb197@users.noreply.github.com>
2026-01-11 15:34:30 +0000

    All the basic components of the API

---
+++ README.md
@@ -0,0 +1,14 @@
+# PyRestarters
+Python Wrappers for the restarters.net API
+
+## Restarters.net
+[restarters.net](https://restarters.net/) is a website widely used within the Repair Community.
+
+### API
+The [API documentation](https://restarters.net/apiv2/documentation) describes the REST API which
+is wrapped by this package
+
+## Getting Started
+
+
+
---
+++ src/pyRestarters/__about__.py
@@ -0,0 +1,20 @@
+"""
+pyRestarters is a python wrapper for the restarters.net API
+Copyright (C) 2026
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Variables that describes the Package
+"""
+__version__ = "0.0.1"
